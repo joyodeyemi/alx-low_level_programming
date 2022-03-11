619f601bdc100a5818c4bb3f638a863f5019f751
joyodeyemi <joybella4eva@gmail.comgit config --global user.email joybella4eva@gmail.comgit config --global user.email joybella4eva@gmail.com>
2022-03-11 15:25:26 -0800

    task 4 done

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
 /**
- * main - main block
- * Description: Use `putchar` to print all letters but the letter 'q' and 'e'.
- * Return: 0
- */
+* main - main block
+* Description: Use `putchar` to print all letters but the letter 'q' and 'e'.
+* Return: 0
+*/
 int main(void)
 {
 	char c = 'a'
